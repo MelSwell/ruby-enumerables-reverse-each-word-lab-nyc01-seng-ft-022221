dd1006bd7e6d23ffb4d7cf437d41f22a90d26a82
Seth Cohen <smcohen82@gmail.com>
2021-02-09 16:15:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,9 @@ def reverse_each_word(str)
   temp_array = str.split(" ")
   temp_array.each do |e|
     reverse_array << e.reverse 
-    
+    reverse_array.join(' ')
   end
-  reverse_array.join(' ')
+  reverse_array
 end
 
 reverse_each_word("Hello my name is Seth")
